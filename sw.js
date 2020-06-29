9498fb4e5d3d81b5c04e65829ef0a4a9bd41b9cc
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-06-29 20:53:59 +0200

    cache app shell

--- sw.js
+++ sw.js
@@ -1,7 +1,35 @@
+"use strict";
+
+const staticCacheName = "site-static-v1.0.0";
+const assets = [
+  "/",
+  "/index.html",
+  "/assets/js/script.js",
+  "/assets/js/materialize.min.js",
+  "/assets/css/style.css",
+  "/assets/css/materialize.min.css",
+  "/assets/css/icons.css",
+  "https://fonts.gstatic.com/s/materialicons/v53/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2",
+];
+
 self.addEventListener("install", (event) => {
-  console.log("service worker has been installed");
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("service worker has been activated");
+  //console.log("service worker has been activated");
+});
+
+self.addEventListener("fetch", (event) => {
+  //console.log('fetch event', event);
+
+  event.respondWith(
+    caches.match(event.request).then((cacheRes) => {
+      return cacheRes || fetch(event.request);
+    })
+  );
 });
